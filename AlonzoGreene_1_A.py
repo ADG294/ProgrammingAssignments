35f8b42c397be3d8bdc611bc1143ef1aca547a22
Alonzo Greene <alonzo.greene29@gmail.com>
2024-08-30 15:53:30 -0400

    Assignment_1_A

---
+++ AlonzoGreene_1_A.py
@@ -0,0 +1,45 @@
+# Program pre-sells tickets up to a certain amount
+
+# then displays number of remaining tickets after prompting
+
+# the user to input how many tickets they want.
+
+# After, app will display total number of buyers.
+
+saleLimit = 4
+
+def buyers():
+
+    preSale = 20
+
+    #numberOfTickets = int(input('Enter number of tickets: '))
+
+    b = 0
+
+
+
+    while preSale != 0:
+
+        numberOfTickets = int(input('Enter number of tickets: '))
+
+        if numberOfTickets > saleLimit:
+            print('Cannot exceed 4 tickets!')
+            numberOfTickets = int(input('Enter number of tickets: '))
+
+        elif preSale == 0:
+            break
+
+        preSale = preSale - numberOfTickets
+
+        b += 1
+
+        print('Remaining Tickets: ', preSale)
+
+    print('Total buyers: ', b)
+
+def main():
+
+    buyers()
+
+main()
+
